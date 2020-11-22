152224093f02d32fa5a9c30efbd4deef2a74a172
Rishabh Tatia <tatiaris2001@gmail.com>
2020-11-22 01:59:33 -0600

    correct binary tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,128 +1,73 @@
 /***************************************************************************
-** File:    Driver.java
+** File:    BinaryTree.java
 ** Project: CSCE 314 Project 1, Fall 2020
 ** Author:  Rishabh Tatia, Sunhee Kim
 ** Date:    11/07/20
 ** Section: 501
 ** E-mail:  tatiaris@tamu.edu, sunheek@tamu.edu
 **
-** This file contains the Driver for our implementation of the merkle tree. 
-** In our implementation, we intend to make authentication at facilities
-** more secure. Our implementation can be used when we want to verify 
-** whether a certain individual is part of the authorized personel 
-** at a facility.
-** 
-** For example: At the GenieBurger facility, to prevent tampering,
-** the secret recipe can only be viewed by 'N' certain individuals when
-** they're all together. The authentication process for all the individuals
-** can be made efficient by implementing the merkle tree into the system.
-** This implementation will generate the merkle root by collecting the
-** data provided by the individuals (could be a password / a file / eye data)
-** and comparing it to the corrext / secure merkle root.
+** This file contains the class BinaryTree, which defines all of the binary
+** tree's functions and operations.
 **
-** The merkle tree structure could further be used to authenticate a single
-** person A, for entering the research room (for new ingredients) where
-** we don't need all N individuals at the same time.
-** 
 ****************************************************************************/
-import java.util.Scanner; 
-import java.util.Map; 
-import java.util.HashMap; 
-// Driver class
-// Main function which will use the merkle tree to implement the security system
-public class Driver {
-    public static void main(String args[])
+// all class imports
+import java.util.*;
+
+public class BinaryTree
+{
+    // Declare class variables here
+    //public ArrayList <Node> TreeList2;
+    public Node [] TreeList;
+   // public Node TreeList;
+    public Node TreeRoot;
+    // BinaryTree (Constructor)
+    // Instantiate the merkle tree
+    public BinaryTree ()
     {
-        MerkleTree correctVersion = new MerkleTree();
-        correctVersion.addUser("A", 7);
-        correctVersion.addUser("B", 8);
-        correctVersion.addUser("C", 9);
-        correctVersion.addUser("D", 10);
-        correctVersion.addUser("E", 11);
-        correctVersion.addUser("F", 12);
-        correctVersion.addUser("G", 13);
-        correctVersion.addUser("H", 14);
-
-        correctVersion.setUsernamePassword("A", "passwordA");
-        correctVersion.setUsernamePassword("B", "passwordB");
-        correctVersion.setUsernamePassword("C", "passwordC");
-        correctVersion.setUsernamePassword("D", "passwordD");
-        correctVersion.setUsernamePassword("E", "passwordE");
-        correctVersion.setUsernamePassword("F", "passwordF");
-        correctVersion.setUsernamePassword("G", "passwordG");
-        correctVersion.setUsernamePassword("H", "passwordH");
-
-        correctVersion.generateMerkleRoot();
-        String correctMerkleRoot = correctVersion.getMerkleRoot();
-        
-        MerkleTree tempVersion = new MerkleTree();
-
-        // options
-        // 1: Enter super secret lab
-        //      requires all 8 passwords
-        //      Enter key: A
-        //      Enter password: Apassword!@#
-        // 
-        // 2: Enter research lab
-        //      verifies one person's password
-        Scanner scan = new Scanner(System.in);
-        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println("Enter Option as Integer (1 or 2)");
-        System.out.println("\tOption 1 (Requires All 8 Password for Verification)");
-        System.out.println("\tOption 2 (Requires Single Password for Verification)");
-        String val = scan.nextLine();
-        while(!val.equals("1") && !val.equals("2"))
-        {
-            System.out.println("ERROR!! INVALID OPTION. Enter Option as Integer (1 or 2)");
-            System.out.println("\tOption 1 (Requires All 8 Password for Verification)");
-            System.out.println("\tOption 2 (Requires Single Password for Verification)");
-            val = scan.nextLine();
-        }
-
-        tempVersion.addUser("A", 7);
-        tempVersion.addUser("B", 8);
-        tempVersion.addUser("C", 9);
-        tempVersion.addUser("D", 10);
-        tempVersion.addUser("E", 11);
-        tempVersion.addUser("F", 12);
-        tempVersion.addUser("G", 13);
-        tempVersion.addUser("H", 14);
-
-
-        Map<String, Integer> x = tempVersion.getKeyValMap();
-        if(val.equals("1"))
-        {
-            for (String name : x.keySet())  
-            {
-
-                System.out.println("Enter the password for the Username "+ name + ": ");
-                val = scan.nextLine();
-                tempVersion.setUsernamePassword("A", val);
-                
-            }
-
-            System.out.println("reach");
-            tempVersion.generateMerkleRoot();
-            
-            if(tempVersion.getMerkleRoot().equals(correctMerkleRoot))
-                System.out.println("Passed all cases for verification");
-            else{
-                System.out.println("Did not pass all cases for verification");
-            }
-            //System.out.println(tempVersion);
-           // tempVersion.setUsername
-        }
-        else{
-            System.out.println("Enter the the Username to verify password for:");
-            String keyVal = scan.nextLine();
-            System.out.println("Enter the password:");
-            val = scan.nextLine();
-
-            //need to copy all values of the the previous corrected version with the exception of the single key specified
-        }
-        
-
+        TreeRoot = new Node(0, "", "");
+        TreeList = new Node[15];
+        TreeList[0] =  TreeRoot;
     }
-}
 
-// -1418649093-1807315620 = -3225964713
+    // insert
+    // Given a key and it's relevant data, create a node
+    // and insert it in the tree
+    void insert(int pos, String key, String hashedData)
+    {
+        Node n = new Node(pos, key, hashedData);
+        TreeList[pos] = n;
+    }
+
+    public String toString() {
+        String data = "";
+        for (int i = 1; i < 5; i++) {
+            for (int j = (int)(Math.pow(2, i-1) - 1); j < (int)(Math.pow(2, i) - 1); j++) {
+                data += TreeList[j].toString() + " ";
+            }
+            data += "\n";
+        }
+        return data;
+    }
+
+    // delete
+    // Given the key to a node, delete the node from the tree
+   /* void delete(String key)
+    {
+        int i = java.util.Arrays.asList(TreeList).indexOf(key)
+    }
+
+    // find
+    // Given the key to a node, return the node
+    Node find(String key)
+    {
+        Node x = new Node();
+        return x;
+    }
+
+    // getSize
+    // Returns the size of the tree
+    int getSize()
+    {
+        return -990;
+    }*/
+}
