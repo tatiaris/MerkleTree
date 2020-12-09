68fbc05c58fd4a4e0ff73e35a3854d276e1bbdf2
SunheeEllisseKim <56172905+SunheeEllisseKim@users.noreply.github.com>
2020-12-08 18:46:07 -0600

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -10,18 +10,15 @@
 ** tree's functions and operations.
 **
 ****************************************************************************/
-
 // all class imports
 import java.util.*;
 
 public class BinaryTree
 {
-    // Declare class variables here
     //public ArrayList <Node> TreeList2;
     public Node [] TreeList;
-    // public Node TreeList;
     public Node TreeRoot;
-    // BinaryTree (Constructor)
+
     // Instantiate the merkle tree
     public BinaryTree ()
     {
@@ -39,7 +36,7 @@ public class BinaryTree
         TreeList[pos] = n;
     }
 
-    // toString function for the class
+    //displays all values belonging to the TreeList variable
     public String toString() {
         String data = "";
         for (int i = 1; i < 5; i++) {
@@ -51,23 +48,4 @@ public class BinaryTree
         return data;
     }
 
-    // delete
-    // Given the key to a node, delete the node from the tree
-   /* void delete(String key)
-    {
-        int i = java.util.Arrays.asList(TreeList).indexOf(key)
-    }
-    // find
-    // Given the key to a node, return the node
-    Node find(String key)
-    {
-        Node x = new Node();
-        return x;
-    }
-    // getSize
-    // Returns the size of the tree
-    int getSize()
-    {
-        return -990;
-    }*/
 }
